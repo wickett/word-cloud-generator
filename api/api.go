82990f301b5da7c138c93de2713ba77ec21e591a
James Wickett <wickett@gmail.com>
2017-05-25 12:00:07 -0500

    updating and build passing

--- api/api.go
+++
@@ -1,7 +0,0 @@
-package wordyapi
-
-import "fmt"
-
-func main() {
-	fmt.Println("howdy")
-}
---
+++ wordyapi/api.go
@@ -0,0 +1,10 @@
+package wordyapi
+
+type TextToParse struct {
+	Title string
+	Text  string
+}
+
+func ParseText(x TextToParse) string {
+	return "hi"
+}
