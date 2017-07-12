9a606fe09d097fa95120e9e6026f60fa5944caa2
James Wickett <wickett@gmail.com>
2017-07-11 22:29:03 -0500

    adding a test for the scoring of occurances

--- wordyapi/api_test.go
+++ wordyapi/api_test.go
@@ -1,10 +1,26 @@
 package wordyapi
 
 import (
+	"encoding/json"
 	"testing"
 )
 
-func TestSpec(t *testing.T) {
-	//	t.Fatal("This test failed")
+func TestParseText(t *testing.T) {
+	//	t.Error("This test failed")
 
+	// Send a string with mixed case
+	s := TextToParse{Title: "Sample", Text: "Test test test Test test test TEST"}
+	out := ParseText(s)
+
+	var v map[string]interface{}
+
+	err := json.Unmarshal(out, &v)
+	if err != nil {
+		t.Error("Got this error:", err)
+	}
+
+	// test to make sure it counted the variations of capitalization
+	if v["test"].(float64) != 7 {
+		t.Error("Expected 7 occurances of test, but got:", v)
+	}
 }
